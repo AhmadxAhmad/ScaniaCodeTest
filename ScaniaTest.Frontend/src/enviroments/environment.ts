104891b30848d664fe489e1368b6bc360232c5bd
AhmadxAhmad <ahmad.x.ahmad@outlook.com>
2023-11-03 11:30:33 +0100

    add configurations to work on http

--- ScaniaTest.Frontend/src/enviroments/environment.ts
+++ ScaniaTest.Frontend/src/enviroments/environment.ts
@@ -3,17 +3,17 @@ export const environment = {
     production: false,
     Vehicle: {
         api: {
-            url: 'https://localhost:7243/api/',
+            url: 'http://localhost:7243/api/',
         }
     },
     Customer: {
           api: {
-                url: 'https://localhost:7185/api/',
+                url: 'http://localhost:7185/api/',
           }
         },
     Simulator: {
               api: {
-                    url: 'https://localhost:7062/api/',
+                    url: 'http://localhost:7062/api/',
               }
             }
 };
